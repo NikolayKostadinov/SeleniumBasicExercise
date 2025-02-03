c303fd75471816d01c26b51f5048093f5090814b
Nikolay.Kostadinov <Kostadinov.Nikolay.d@gmail.com>
2025-02-03 09:17:42 +0200

    path fix

--- TestProject3/DropDown.cs
+++ TestProject3/DropDown.cs
@@ -15,10 +15,15 @@ namespace TestProject3
         [SetUp]
         public void SetUp()
         {
-            // Create object of ChromeDriver
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("headless");
+            options.AddArgument("no-sandbox");
+            options.AddArgument("disable-dev-shm-usage");
+            options.AddArgument("disable-gpu");
+            options.AddArgument("window-size=1920,1080");
+            options.AddArgument("disable-extensions");
 
-            // Add implicit wait
+            driver = new ChromeDriver(options);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
         }
 
